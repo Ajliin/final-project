540408e00bd49b64a3e32b47994825204ba3b944
Elin Elmvik Diczfalusy <elinelmvik@gmail.com>
2022-01-24 09:32:36 +0100

    build: rearrange endpointsso all are connected to user (both myPage and Company) and added a get all companies endpoint

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ mongoose.connect(mongoUrl, {
 })
 mongoose.Promise = Promise
 
+// *************** Schemas **************
+
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -35,26 +37,20 @@ const UserSchema = new mongoose.Schema({
     type: String,
     default: () => crypto.randomBytes(128).toString('hex'),
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'myPage',
-  },
-  company: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Company',
-  },
 })
 
-const User = mongoose.model('User', UserSchema)
-
+// MY PAGE
 const MyPageSchema = new mongoose.Schema({
   description: {
     type: String,
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 })
 
-const MyPage = mongoose.model('myPage', MyPageSchema)
-
+// COMPANY
 const CompanySchema = new mongoose.Schema({
   companyName: {
     type: String,
@@ -62,12 +58,22 @@ const CompanySchema = new mongoose.Schema({
   genderQuote: {
     type: Number,
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 })
 
+// ************** Models ****************
+
+const User = mongoose.model('User', UserSchema)
+const MyPage = mongoose.model('myPage', MyPageSchema)
 const Company = mongoose.model('Company', CompanySchema)
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
+
+// ************** Middleware ****************
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -75,131 +81,34 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// const authenticateMyPage = async (req, res, next) => {
-//   const accessToken = req.header('Authorization')
+const authenticateMyPage = async (req, res, next) => {
+  const accessToken = req.header('Authorization')
 
-//   try {
-//     const company = await MyPage.findOne({ accessToken })
-//     if (company) {
-//       next()
-//     } else {
-//       res.status(401).json({
-//         response: {
-//           message: 'Please, log in',
-//         },
-//         success: false,
-//       })
-//     }
-//   } catch (error) {
-//     res.status(400).json({ response: error, success: false })
-//   }
-// }
+  try {
+    const user = await User.findOne({ accessToken })
+    console.log(user)
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({
+        response: {
+          message: 'Please, log in',
+        },
+        success: false,
+      })
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+}
 
 // Authentication = member - 401
 // Authorization = what type of member - 403
 
 // Start defining your routes here
 // app.get('/thoughts', authenticateMyPage)
-
-app.post('/profile', async (req, res) => {
-  const { description } = req.body
-
-  try {
-    const newMyPage = await new MyPage({ description }).save()
-    res.status(201).json({ response: newMyPage, success: true })
-  } catch (error) {
-    res.status(400).json({ response: error, success: false })
-  }
-})
-
-app.post('/user', async (req, res) => {
-  const { username, user } = req.body
-
-  try {
-    const queriedUser = await MyPage.findById(user)
-    const newUser = await new User({ username, user: queriedUser }).save()
-
-    res.status(201).json({ response: newUser, success: true })
-  } catch (error) {
-    res.status(400).json({ response: error, success: false })
-  }
-})
-
-app.get('/user/:userId', async (req, res) => {
-  const { userId } = req.params
-
-  const user = await User.findById(userId).populate('user')
-  res.status(200).json({ response: user, success: true })
-})
-
-// ****************** COMPANY ***********************
-app.post('/companyprofile', async (req, res) => {
-  const { companyName, genderQuote } = req.body
-
-  try {
-    const newCompany = await new Company({ companyName, genderQuote }).save()
-
-    res.status(201).json({ response: newCompany, success: true })
-  } catch (error) {
-    res.status(400).json({ response: error, success: false })
-  }
-})
-
-app.patch('/company', async (req, res) => {
-  const { username, company } = req.body
-
-  try {
-    const queriedCompany = await Company.findById(company)
-    console.log('queierd', queriedCompany)
-    const newCompany = await new User({
-      username,
-      company: queriedCompany,
-    }).save()
-    console.log('indata new compnay', newCompany)
-
-    res.status(201).json({ response: newCompany, success: true })
-  } catch (error) {
-    res.status(400).json({ response: error, success: false })
-  }
-})
-
-app.get('/company/:companyId', async (req, res) => {
-  const { companyId } = req.params
-
-  try {
-    const company1 = await User.findById(companyId).populate('company')
-    res.status(200).json({ response: company1, success: true })
-  } catch (error) {
-    res.status(400).json({ response: error, success: false })
-  }
-})
-
-// app.post('/profile', async (req, res) => {
-//   const { userName, myPage } = req.body
-
-//   try {
-//     const queriedUser = await User.findById(myPage)
-//     console.log(queriedUser)
-//     const newUser = await new MyPage({
-//       userName,
-//       myPage: queriedUser,
-//     }).save()
-//     console.log('newUser', newUser)
-//     res.status(201).json({ response: newUser, success: true })
-//   } catch (error) {
-//     res.status(400).json({ response: error, success: false })
-//   }
-// })
-
-// app.get('/profile/:userID', async (req, res) => {
-//   const { userId } = req.params
-
-//   const user = await User.findById(userId).populate('company')
-//   //company = the proporty in row 66
-//   res.status(201).json({ response: user, success: true })
-// })
-
 //Signup endpoint
+
 app.post('/signup', async (req, res) => {
   const { username, password, email } = req.body
 
@@ -219,8 +128,8 @@ app.post('/signup', async (req, res) => {
     res.status(201).json({
       response: {
         userId: newUser._id,
-        username,
-        email,
+        username: newUser.username,
+        email: newUser.email,
         accessToken: newUser.accessToken,
       },
       success: true,
@@ -231,32 +140,110 @@ app.post('/signup', async (req, res) => {
 })
 
 // Signin endpoint
-// app.post('/signin', async (req, res) => {
-//   const { username, password } = req.body
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body
 
-//   try {
-//     const user = await User.findOne({ username })
+  try {
+    const user = await User.findOne({ email })
 
-//     if (user && bcrypt.compareSync(password, user.password)) {
-//       res.status(200).json({
-//         response: {
-//           userId: user._id,
-//           username: user.username,
-//           email: user.email,
-//           accessToken: user.accessToken,
-//         },
-//         success: true,
-//       })
-//     } else {
-//       res.status(404).json({
-//         response: "Username or password doesn't match",
-//         success: false,
-//       })
-//     }
-//   } catch (error) {
-//     res.status(400).json({ response: error, success: false })
-//   }
-// })
+    if (email && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        response: {
+          userId: user._id,
+          username: user.username,
+          email: user.email,
+          accessToken: user.accessToken,
+        },
+        success: true,
+      })
+    } else {
+      res.status(404).json({
+        response: "Username or password doesn't match",
+        success: false,
+      })
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+})
+
+//endpoint for creting a new myPage
+//app.post('/profile', authenticateMyPage)
+app.post('/profile', async (req, res) => {
+  const { description, user } = req.body
+
+  try {
+    const queriedUser = await User.findById(user)
+    console.log(queriedUser)
+    const newUser = await new MyPage({
+      description: description,
+      user: queriedUser,
+    }).save()
+    console.log('newUser', newUser)
+
+    res.status(201).json({ response: newUser, success: true })
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+})
+
+//to get "all" profile- maybe not needed later, will just be one and prob. patch?
+app.get('/profile/:userId', async (req, res) => {
+  const { userId } = req.params
+
+  try {
+    const profile = await MyPage.find({ user: userId }).populate('user')
+    console.log('inside ge - profile', profile)
+    res.status(200).json({ response: profile, success: true })
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+})
+
+// ****************** COMPANY ***********************
+
+//to create new company
+app.post('/company', async (req, res) => {
+  const { companyName, genderQuote, user } = req.body
+
+  try {
+    const queriedUser = await User.findById(user)
+    const newCompany = await new Company({
+      companyName,
+      genderQuote,
+      user: queriedUser,
+    }).save()
+
+    res.status(201).json({ response: newCompany, success: true })
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+})
+
+// to get all companies from one unser
+
+app.get('/company/:userId', async (req, res) => {
+  const { userId } = req.params
+
+  console.log(Company.companyName)
+
+  try {
+    const company = await Company.find({ user: userId }).populate('user')
+    res.status(200).json({ response: company, success: true })
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+})
+
+// to get all companies
+app.get('/allcompanies', async (req, res) => {
+  try {
+    const companies = await Company.find({})
+    res.status(200).json({ response: companies, success: true })
+  } catch (error) {
+    res.status(400).json({ response: error, success: false })
+  }
+})
 
 // Start the server
 app.listen(port, () => {
